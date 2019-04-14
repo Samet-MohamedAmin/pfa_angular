6a8b20f142f18ec72537b9108a742008cd78786c
Amin Bm <medaminbenmbarek@gmail.com>
2019-04-14 22:54:05 +0100

    styling of branches in home page

--- src/app/home/branch-list/branch-list.component.ts
+++ src/app/home/branch-list/branch-list.component.ts
@@ -6,24 +6,48 @@ import { BranchItemInterface } from '../branch-item/branch-item.interface';
   templateUrl: './branch-list.component.html',
   styleUrls: ['./branch-list.component.css']
 })
-export class BranchListComponent implements OnInit {
+export class BranchListComponent  {
 
-  branchList: BranchItemInterface[] = [];
-  dummyText = 'Lorem ipsum dolor sit amet consectetur adipiscing';
+  branchList: BranchItemInterface[] = [{
+    name: 'Génie Logiciel (GL)',
+    image: 'assets/images/computer_01.jpg',
+    imageAlt: 'GL_IMAGE',
+    description:"La filière Génie Logiciel a pour objectif de former des ingénieurs maîtrisant les méthodes d’analyse et de conduite de projets informatiques ainsi que les langages et les outils menant au développement de logiciels."
+  },
+  {
+    name: 'Informatique Industrielle & Automatique (IIA)',
+    image: 'assets/images/computer_01.jpg',
+    imageAlt: 'GL_IMAGE',
+    description:"La filière Informatique industrielle et Automatique a pour objectif de former des ingénieurs aptes à maîtriser les outils de production automatisées s’appuyant sur des techniques informatiques de pointe (système temps réel, contrôle, commande, robotique, etc.)"
+  },
+  {
+    name: 'Instrumentation et Maintenance Industrielle (IMI)',
+    image: 'assets/images/computer_01.jpg',
+    imageAlt: 'GL_IMAGE',
+    description:"La filière Instrumentation et Maintenance Industrielle a pour objectif de former des ingénieurs capables d’analyser et de traiter de nombreuses mesures imposées par les technologies avancées et assurer dans les entreprises les fonctions de maintenance des installations de fabrication, des processus de production, de la qualité d’un produit et de son entretien."
+  },
+  {
+    name: 'Réseaux Informatiques et Télécommunications (RT)',
+    image: 'assets/images/computer_01.jpg',
+    imageAlt: 'GL_IMAGE',
+    description:"La filière Réseaux Informatiques et Télécommunications a pour objectif de former des ingénieurs dans les domaines de l’informatique intégrant les moyens de communication dans les différents aspects de transmission de données et les organisations en réseau."
+  },
+  {
+    name: 'Chimie Industrielle (CH)',
+    image: 'assets/images/computer_01.jpg',
+    imageAlt: 'GL_IMAGE',
+    description:"La filière Chimie Industrielle a pour objectif la formation d’ingénieurs aptes à conduire des activités d’analyses physico-chimiques et d’élaboration de matériaux."
+  },
+  {
+    name: 'Biologie industrielle (BIO)',
+    image: 'assets/images/computer_01.jpg',
+    imageAlt: 'GL_IMAGE',
+    description:"La filière Biologie industrielle a pour objectif de former des ingénieurs aptes à maîtriser les méthodes d’analyses chimiques et biologiques, dans un secteur industriel, agroalimentaire ou pharmaceutique."
+  }];
 
   constructor() { }
 
-  ngOnInit() {
-    for (let i = 0; i < 6; i++) {
-      const branchItem: BranchItemInterface = {
-        name: 'Génie Logiciel',
-        image: 'assets/images/computer_01.jpg',
-        imageAlt: 'GL_IMAGE',
-        description: this.dummyText
-      };
+  
 
-      this.branchList.push(branchItem);
-    }
-  }
 
 }
