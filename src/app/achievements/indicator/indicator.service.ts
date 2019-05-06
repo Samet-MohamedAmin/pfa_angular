a753f1829e15f655cb2fbab15f2ebbe495f70a83
mohamedamin <mohamedamin.samet@gmail.com>
2019-05-06 23:05:32 +0100

    add indicator service

---
+++ src/app/achievements/indicator/indicator.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { environment } from '@4c-environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IndicatorService {
+
+  baseUrl = environment.BACKEND_URL + '/indicator';
+
+  constructor(private http: HttpClient){}
+
+
+  getAllIndicators(): Observable<any>{
+    return this.http.get<any>(this.baseUrl);
+  }
+
+}
