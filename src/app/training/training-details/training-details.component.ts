56d393dab9aa4f95d7af866b5cd34fef9d1790e9
mohamedamin <mohamedamin.samet@gmail.com>
2019-05-24 07:47:29 +0100

    training rating

--- src/app/training/training-details/training-details.component.ts
+++ src/app/training/training-details/training-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { SharedService, TrainingRequestState } from '@4c-shared/shared.service';
 import { ActivatedRoute } from '@angular/router';
 import { TrainingService } from '../training.service';
@@ -14,7 +14,7 @@ export class TrainingDetailsComponent implements OnInit {
 
   //this is the real training object to use after that
   training: TrainingItemInterface;
-  user: any = null;
+  userDetails: any = null;
 
   // dummyBranchTreeList = ['Informatique', 'DevOps', 'Test Logiciel'];
 
@@ -22,22 +22,19 @@ export class TrainingDetailsComponent implements OnInit {
   Lorem ipsum dolor sit amet consectetur adipiscing elit imperdiet pretium congue egestas, enim convallis libero montes senectus vestibulum nisl dis habitant. Conubia nibh vestibulum id sagittis augue felis dictum rhoncus, condimentum eros eget sem ut lobortis enim, est porttitor porta himenaeos rutrum cum neque. Per sociosqu maecenas orci rutrum velit molestie aenean, quam interdum magnis aliquet senectus leo nibh sed, pellentesque scelerisque fermentum cursus netus magna. Eget primis neque euismod est molestie rutrum blandit venenatis rhoncus, aptent volutpat elementum lacus cum facilisis platea cubilia, consequat sapien egestas ante risus imperdiet nec magna.
   Inceptos nisl viverra blandit volutpat sollicitudin nostra non condimentum sagittis, id vivamus vehicula hac eget turpis rhoncus aptent magnis in, morbi netus aliquet est quisque vestibulum feugiat penatibus. Convallis curae volutpat accumsan ut per imperdiet eu quis, odio libero lacus sagittis nec cras duis tristique iaculis, nunc massa tempus suscipit tempor parturient vehicula. Nisi diam sollicitudin ante parturient ultrices hendrerit in suscipit nostra felis rutrum, condimentum posuere egestas cursus nam magnis integer pellentesque morbi elementum. Nisi malesuada non vestibulum inceptos nam vel convallis ultrices hac, sollicitudin porttitor gravida tempus duis torquent nulla nascetur, odio litora orci fusce proin ut curabitur mus.`
 
-  preList = [
-    'Lorem ipsum dolor sit amet consectetur adipiscing elit imperdiet pretium congue egestas, enim convallis libero montes.',
-    'Lorem ipsum dolor sit amet consectetur adipiscing elit imperdiet pretium congue egestas,',
-    'Lorem ipsum dolor sit amet consectetur adipiscing elit imperdiet pretium congue egestas, enim convallis libero montes senectus vestibulum nisl dis habitant.'
-  ]
 
-  rating:number;
+  // TODO: rating value must be binded directely with training object
   starList = [];
   levels = TRAINING_LEVEL;
   
   trainingRequestState: TrainingRequestState = null;
 
+  @ViewChild('starsContainer') starsContainer: ElementRef;
+
   constructor(private route:ActivatedRoute,
               private auth: AuthenticationService,
               private trainingService:TrainingService) {
-    this.user = this.auth.getUserDetails();
+    this.userDetails = this.auth.getUserDetails();
     this.route.params
     .subscribe(params=>{
       this.trainingService.getTrainingDetail(params.id)
@@ -45,23 +42,23 @@ export class TrainingDetailsComponent implements OnInit {
         console.log(training);
         
         this.training=this.trainingService.mapTraining(training);
-        this.getTrainingRequestState()
+        this.getTrainingRequestState();
+        this.generateStars();
       })
     })
    }
 
   ngOnInit() {
-    this.generateStars();
-
+    console.log(this.userDetails.role);
   }
 
   getTrainingRequestState(){
-    if(this.user) {
-      if(this.training.attendees && this.training.attendees.includes(this.user._id)){
+    if(this.userDetails) {
+      if(this.training.attendees && this.training.attendees.includes(this.userDetails._id)){
         this.trainingRequestState = 'ACCEPTED';
       }
       else {
-        this.trainingService.getUserRequests(this.user._id).subscribe(
+        this.trainingService.getUserRequests(this.userDetails._id).subscribe(
           (registrationRequestList: any[]) => {
             const request: any = registrationRequestList
                                   .find(reg => reg.courseId == this.training._id)
@@ -75,17 +72,32 @@ export class TrainingDetailsComponent implements OnInit {
       }
     }
   }
+
+  rateTraining(eventData){
+    const offsetLeft = this.starsContainer.nativeElement.offsetLeft;
+    const offsetWidth = this.starsContainer.nativeElement.offsetWidth;
+    const pageX = eventData.pageX;
+    this.training.globalRating = Math.round((pageX - offsetLeft) * 10 / offsetWidth);
+
+    this.updateTraining();
+
+    this.generateStars();
+  }
+  
   generateStars(){
-    this.rating = SharedService.getRandomNumber(4, 11);
-    console.log(`rating: ${this.rating}`);
+    // this.rating = SharedService.getRandomNumber(4, 11);
+    // TODO: starList elements must be removed to free memory
+    this.starList = [];
+
+    console.log(`rating: ${this.training.globalRating}`);
     let starFull = 'star';
     let starHalf = 'star_half';
     let starEmpty = 'star_border';
 
-    for(let i=0; i<Math.floor(this.rating/2); i++){
+    for(let i=0; i<Math.floor(this.training.globalRating/2); i++){
       this.starList.push(starFull);
     }
-    if(this.rating%2) this.starList.push(starHalf);
+    if(this.training.globalRating%2) this.starList.push(starHalf);
     let starEmptyNumber = 5 - this.starList.length;
     for(let i=0; i<starEmptyNumber; i++){
       this.starList.push(starEmpty);
@@ -95,12 +107,16 @@ export class TrainingDetailsComponent implements OnInit {
 
   registerRequest(){
     if(this.trainingRequestState === null){
-      this.trainingService.requestRegistration(this.user.type,this.user._id, this.training._id).subscribe(
-        obj => console.log(obj)
-      );
-
+      this.updateTraining();
+      
       this.trainingRequestState = 'PENDING';
     }
+  }
+
+  updateTraining(){
+    this.trainingService
+          .requestRegistration(this.userDetails.role,this.userDetails._id, this.training._id)
+          .subscribe(obj => {console.log('update training'); console.log(obj)});
   }
 
 }
