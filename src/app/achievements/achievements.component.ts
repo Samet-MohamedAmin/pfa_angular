a9726a12f7a735031834bbd2561ae44cf1e0c518
mohamedamin <mohamedamin.samet@gmail.com>
2019-05-29 02:04:02 +0100

    use real indicators

--- src/app/achievements/achievements.component.ts
+++ src/app/achievements/achievements.component.ts
@@ -14,40 +14,51 @@ import { IndicatorService } from './indicator/indicator.service';
 export class AchievementsComponent implements OnInit {
   indicatorList: IndicatorInterface[] = [];
   colorList: IndicatorColor[] = COLOR_LIST;
-  iconList: string[] = ['all_inbox',
-                        'check_circle',
-                        'donut_large',
-                        'archive',
-                        'cached',
-                        'done',
-                        'favorite',
-                        'sentiment_satisfied_alt',
-                        'star',
-                        'school',
-                        'mood']
+  iconBaseUrl:string = 'assets/icons/indicators/';
+  iconExtension: string = '.png';
+  iconList: string[] = ['indicator_01_certified_students',
+                        'indicator_02_certified_teachers',
+                        'indicator_03_formed_females',
+                        'indicator_04_formed_students',
+                        'indicator_05_formed_teachers',
+                        'indicator_06_total_actions',
+                        'indicator_07_training_planified',
+                        'indicator_08_training_realized']
      
   constructor(private indicatorService:IndicatorService){
-  this.indicatorService.getAllIndicators()
-  .subscribe(indicators=>{
-    console.log(indicators)
-  })
+    this.indicatorService.getAllIndicators()
+    .subscribe((indicators: {})=>{
+      console.log(indicators);
+      const keyList: string[] = Object.keys(indicators).sort();
+      keyList.forEach((key: string)=>{
+        if(!key.startsWith('_')) {
+          const indicator: IndicatorInterface = this.mapIndicator(key,
+                                                                  indicators[key],
+                                                                  this.indicatorList.length);
+          this.indicatorList.push(indicator);
+        }
+      });
+      console.log(this.indicatorList);
+    })
   }                      
   /**
    * initialize `indicatorList`
    */
   ngOnInit() {
-    
-    for(let i=0; i<6; i++){
-      const indicator: IndicatorInterface = {
-        name: 'Dummy Indicator',
-        value: '20',
-        goalCurrent: {value: '50', dueTo: '2019'},
-        goalNext: '80 pour 2020',
-        color: SharedService.getRandomObject(COLOR_LIST),
-        icon: SharedService.getRandomObject(this.iconList)
-      }
-      this.indicatorList.push(indicator);
+  }
+
+  mapIndicator(indicatorName: string,
+              indicatorValue: string,
+              iconIndex: number): IndicatorInterface{
+    const indicator: IndicatorInterface = {
+      name: indicatorName,
+      value: indicatorValue,
+      goalCurrent: {value: '50', dueTo: '2019'},
+      goalNext: '80 pour 2020',
+      color: COLOR_LIST[iconIndex],
+      icon: this.iconBaseUrl + this.iconList[iconIndex] + this.iconExtension
     }
+    return indicator;
   }
 
 }
--- src/app/achievements/indicator/indicator.interface.ts
+++ src/app/achievements/indicator/indicator.interface.ts
@@ -7,5 +7,18 @@ export interface IndicatorInterface {
     icon: string;
 }
 
-export type IndicatorColor = '#F44336' | '#E91E63' | '#9C27B0' | '#673AB7' | '#009688' | '#4CAF50' | '#E65100' | '#FFC107' | '#FF3D00' | '#FBC02D' | '#1E88E5' | '#01579B' | '#006064';
-export const COLOR_LIST: IndicatorColor[] = ['#F44336', '#E91E63', '#9C27B0', '#673AB7', '#009688', '#4CAF50', '#E65100', '#FFC107', '#FF3D00', '#FBC02D', '#1E88E5', '#01579B', '#006064'];
+const selectedColors = {
+                        certifedStudent: 'blue',
+                        certifedTeacher: 'violet | deep_blue',
+                        formedFemale: 'pink | violet',
+                        formedStudents: 'green',
+                        formedTeachers: 'deep_green',
+                        actions: 'gold',
+                        planified: 'deep_orange',
+                        realized: 'brown'
+                    };
+
+export type IndicatorColor = string//'#F44336' | '#E91E63' | '#9C27B0' | '#673AB7' | '#009688' | '#4CAF50' | '#E65100' | '#FFC107' | '#FF3D00' | '#FBC02D' | '#1E88E5' | '#01579B' | '#006064';
+
+
+export const COLOR_LIST: IndicatorColor[] = ['#1E88E5', '#01579B', '#9C27B0', '#43A047', '#33691E', '#FFAB00', '#E65100', '#795548'];
