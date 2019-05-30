a8abfd653e456ad2cab3aa518cf5a2481d8feffc
mohamedamin <mohamedamin.samet@gmail.com>
2019-05-30 05:21:38 +0100

    image upload preview

--- src/app/training/training-create/training-create-description/training-create-description.component.ts
+++ src/app/training/training-create/training-create-description/training-create-description.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input,EventEmitter,Output} from '@angular/core';
+import { Component, Input,EventEmitter,Output, ViewChild, ElementRef} from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { MatFormFieldAppearance } from '@angular/material';
 
@@ -7,18 +7,21 @@ import { MatFormFieldAppearance } from '@angular/material';
   templateUrl: './training-create-description.component.html',
   styleUrls: ['./training-create-description.component.css']
 })
-export class TrainingCreateDescriptionComponent implements OnInit {
+export class TrainingCreateDescriptionComponent {
   @Output() imageEmitter =new EventEmitter<any>();
   @Input() formGroupDescription: FormGroup;
   @Input() formFieldAppearance: MatFormFieldAppearance;
 
+  @ViewChild('inputImageContainer') inputImageContainer: ElementRef;
+
   constructor() { }
 
-  ngOnInit() {
-  }
-
   onFileSelected(event){
-  this.imageEmitter.emit(event.target.files[0])
+    const image:any = event.target.files[0];
+    const imageSrc = URL.createObjectURL(image);
+    this.inputImageContainer.nativeElement.style.backgroundImage = `url(${imageSrc})`;
+    console.log(imageSrc)
+    this.imageEmitter.emit(image);
   }
 
 }
